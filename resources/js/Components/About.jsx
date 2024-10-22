10bc19e77d81a309ade610f96ef8d47445ada71e
BowoNugroho <bowotkj10@gmail.com>
2024-10-22 09:05:13 +0600

    update

---
+++ resources/js/Components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="about">
+            <div className="container mx-auto py-2">
+                <div className="pt-10">Tentang Kami</div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- resources/js/Pages/Web.jsx
+++ resources/js/Pages/Web.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "@/Components/Navbar";
 import Home from "../Components/Home";
+import About from "../Components/About";
 
 function Web() {
     return (
         <div>
             <Navbar />
             <Home />
+            <About />
         </div>
     );
 }
