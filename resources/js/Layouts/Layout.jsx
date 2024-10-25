afcfa1f4d377df1b21a52e6d8b9acaf853fc0c6d
BowoNugroho <bowotkj10@gmail.com>
2024-10-25 21:40:22 +0600

    update

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -3,6 +3,10 @@ import wa from "@/Assets/wa.png";
 import { Link } from "react-scroll";
 import { TiThMenuOutline } from "react-icons/ti";
 import { IoMdCloseCircleOutline } from "react-icons/io";
+import { SiGooglemaps, SiGmail, SiWhatsapp, SiInstagram } from "react-icons/si";
+import { BsTelephoneFill } from "react-icons/bs";
+import { AiOutlineGlobal, AiOutlineCopyright } from "react-icons/ai";
+import { FaFacebookF, FaYoutube } from "react-icons/fa6";
 
 const Layout = ({ children }) => {
     const [nav, setNav] = useState(false);
@@ -24,12 +28,8 @@ const Layout = ({ children }) => {
         <>
             <div className="grid grid-cols-1 gap-4">
                 <div className="flex items-center  w-full  text-[11px] px-5 lg:px-[117px] py-2  bg-orange-400">
-                    <img
-                        src={wa}
-                        alt=""
-                        className="w-[20px] h-[15] object-contain "
-                    />
-                    <p>HUBUNGI KAMI : 087485416954</p>
+                    <SiWhatsapp className="mr-2 text-white" />
+                    <p className="text-white"> HUBUNGI KAMI : 087485416954</p>
                 </div>
                 <div
                     className={`flex justify-between items-center px-5 lg:px-[120px] h-[60px] w-full fixed ${scrollActive}`}
@@ -127,7 +127,75 @@ const Layout = ({ children }) => {
                     </div>
                 </div>
             </div>
-            <main>{children}</main>
+            <main className="mb-10">{children}</main>
+            <div className="bg-gray-500 ">
+                <div className="container mx-auto py-2">
+                    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 pt-14 px-5">
+                        <div className="box">
+                            <span className="font-bold text-white lg:text-2xl text-xl px-10">
+                                Ridwan Janur
+                            </span>
+                            <p className="text-white lg:text-lg md:text-md  text-sm px-10 mt-5">
+                                Kami melayani pesanan anyaman Janur, Dekorasi
+                                Bunga Siraman / Wedding, Tuwuhan, Rajut Melati
+                                CPW, Rias Busana , MC & Pemandu dan berbagai
+                                macam kebutuhan wedding.
+                            </p>
+                        </div>
+                        <div className=" box  text-white lg:text-lg md:text-md text-sm px-10 ">
+                            <span className="font-bold ">Kontak Kami</span>
+                            <p className=" flex mt-5 ">
+                                <SiGooglemaps className="mr-3 text-2xl" />
+                                Jln pintu pasar barat losjanur no 8 balimester
+                                jatinegara 13310
+                            </p>
+                            <p className=" flex">
+                                <SiGmail className="mr-3 h-6 text-md" />
+                                ridwanjanur@gmail.com
+                            </p>
+                            <p className=" flex">
+                                <BsTelephoneFill className="mr-3 h-6 text-md" />
+                                0813 9373 0949
+                            </p>
+                            <p className=" flex">
+                                <SiWhatsapp className="mr-3 h-6 text-md" />
+                                0813 9373 0949
+                            </p>
+                            <p className=" flex">
+                                <AiOutlineGlobal className="mr-3 h-7 text-lg" />
+                                www.ridwanjanur.com
+                            </p>
+                        </div>
+                        <div className="box  text-white lg:text-lg md:text-md text-sm px-10 mb-5">
+                            <span className="font-bold ">Layanan Kami</span>
+                            <p className="mt-5">
+                                Dekorasi Bunga Gedung / Wedding
+                            </p>
+                            <p>Dekorasi Bunga Siraman</p>
+                            <p>Pesanan Anyaman Janur</p>
+                            <p>Tuwuhan</p>
+                            <p>Rajut Melati CPW</p>
+                            <p>Rias Busana</p>
+                            <p>MC & Pemandu</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-gray-600 ">
+                <div className="container mx-auto py-2">
+                    <div className="flex justify-between pt-5 px-5">
+                        <p className="flex  text-white px-5 lg:text-lg md:text-md text-[12px]">
+                            <AiOutlineCopyright className="lg:h-6 h-4 text-lg" />
+                            2024 ridwanjanur.com. All rights reserved.
+                        </p>
+                        <div className="flex gap-4 text-white px-5 lg:text-lg md:text-md text-[12px]">
+                            <SiInstagram />
+                            <FaFacebookF />
+                            <FaYoutube />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
