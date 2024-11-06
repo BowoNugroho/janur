728fb20fa38b7d4c73e22c6c5d14f822220dae25
BowoNugroho <bowotkj10@gmail.com>
2024-11-06 09:32:12 +0600

    update

--- resources/js/Pages/Product.jsx
+++ resources/js/Pages/Product.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../Layouts/Layout";
 import dekor from "@/Assets/dekor.jpg";
 import home from "@/Assets/home.jpeg";
@@ -8,14 +8,37 @@ import janur from "@/Assets/janur.jpg";
 import rajut from "@/Assets/rajut.jpg";
 import rias from "@/Assets/rias.jpg";
 import mc from "@/Assets/mc.jpeg";
+import { motion } from "framer-motion";
 
 const Product = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isScrolled2, setIsScrolled2] = useState(false);
+    const handleScroll = () => {
+        // Memeriksa apakah sudah menggulir ke bawah
+        if (window.scrollY > 100) {
+            setIsScrolled(true);
+        } else {
+            // setIsScrolled(false);
+        }
+    };
+    const handleScroll2 = () => {
+        // Memeriksa apakah sudah menggulir ke bawah
+        if (window.scrollY > 400) {
+            setIsScrolled2(true);
+        } else {
+            // setIsScrolled(false);
+        }
+    };
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", handleScroll2);
+    }, []);
     return (
         <Layout>
             <div className="product">
                 <div className="pt-24">
                     <div
-                        className="h-[400px] w-full bg-center bg-inherit"
+                        className="lg:h-[300px] h-[250px] w-full bg-center bg-inherit"
                         style={{
                             backgroundImage: `url(${dekor})`,
                             backgroundSize: `2000px`,
@@ -29,7 +52,12 @@ const Product = () => {
                         </p>
                     </div>
                     <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 pt-5 px-5">
-                        <div className="box bg-gray-100 rounded-2xl">
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 1 }}
+                            className="box bg-gray-100 rounded-2xl"
+                        >
                             <div className="flex justify-center pt-10">
                                 <img
                                     src={home}
@@ -40,8 +68,13 @@ const Product = () => {
                             <div className="flex justify-center pt-5  font-bold mb-10">
                                 <p>Dekorasi Bunga Gedung</p>
                             </div>
-                        </div>
-                        <div className="box bg-gray-100 rounded-2xl">
+                        </motion.div>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 1 }}
+                            className="box bg-gray-100 rounded-2xl"
+                        >
                             <div className="flex justify-center pt-10">
                                 <img
                                     src={siraman}
@@ -52,8 +85,13 @@ const Product = () => {
                             <div className="flex justify-center pt-5  font-bold mb-10">
                                 <p>Dekorasi Bunga Siraman</p>
                             </div>
-                        </div>
-                        <div className="box bg-gray-100 rounded-2xl">
+                        </motion.div>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 1 }}
+                            className="box bg-gray-100 rounded-2xl"
+                        >
                             <div className="flex justify-center pt-10">
                                 <img
                                     src={janur}
@@ -64,8 +102,19 @@ const Product = () => {
                             <div className="flex justify-center pt-5  font-bold mb-10">
                                 <p>Berbagai Anyaman Janur</p>
                             </div>
-                        </div>
-                        <div className="box bg-gray-100 rounded-2xl">
+                        </motion.div>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={
+                                isScrolled
+                                    ? { opacity: 1, scale: 1 }
+                                    : { opacity: 0, scale: 0 }
+                            }
+                            transition={{
+                                duration: 1,
+                            }}
+                            className="box bg-gray-100 rounded-2xl"
+                        >
                             <div className="flex justify-center pt-10">
                                 <img
                                     src={tuwuhan}
@@ -76,8 +125,19 @@ const Product = () => {
                             <div className="flex justify-center pt-5  font-bold mb-10">
                                 <p>Tuwuhan</p>
                             </div>
-                        </div>
-                        <div className="box bg-gray-100 rounded-2xl">
+                        </motion.div>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={
+                                isScrolled
+                                    ? { opacity: 1, scale: 1 }
+                                    : { opacity: 0, scale: 0 }
+                            }
+                            transition={{
+                                duration: 1,
+                            }}
+                            className="box bg-gray-100 rounded-2xl"
+                        >
                             <div className="flex justify-center pt-10">
                                 <img
                                     src={rajut}
@@ -88,8 +148,19 @@ const Product = () => {
                             <div className="flex justify-center pt-5  font-bold mb-10">
                                 <p>Rajut Melati</p>
                             </div>
-                        </div>
-                        <div className="box bg-gray-100 rounded-2xl">
+                        </motion.div>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={
+                                isScrolled
+                                    ? { opacity: 1, scale: 1 }
+                                    : { opacity: 0, scale: 0 }
+                            }
+                            transition={{
+                                duration: 1,
+                            }}
+                            className="box bg-gray-100 rounded-2xl"
+                        >
                             <div className="flex justify-center pt-10">
                                 <img
                                     src={rias}
@@ -100,8 +171,19 @@ const Product = () => {
                             <div className="flex justify-center pt-5  font-bold mb-10">
                                 <p>Rias Busana</p>
                             </div>
-                        </div>
-                        <div className="box bg-gray-100 rounded-2xl">
+                        </motion.div>
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={
+                                isScrolled2
+                                    ? { opacity: 1, scale: 1 }
+                                    : { opacity: 0, scale: 0 }
+                            }
+                            transition={{
+                                duration: 1,
+                            }}
+                            className="box bg-gray-100 rounded-2xl"
+                        >
                             <div className="flex justify-center pt-10">
                                 <img
                                     src={mc}
@@ -112,7 +194,7 @@ const Product = () => {
                             <div className="flex justify-center pt-5  font-bold mb-10">
                                 <p>MC dan Pemandu</p>
                             </div>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
